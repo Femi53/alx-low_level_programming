d8dfc7e374f2dde1cc048c1a71650d1f6db64a4b
Oluwafemi Babalola <oluwafemibabalolae@gmail.com>
2022-07-08 04:12:52 -0700

    there you are

--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "main.h"
 /**
  * main - Entry point
  * Return: Always 0.
  */
 int main(void)
 {
-	printf("_putchar\n");
+	print_alphabet();
 	return (0);
 }
